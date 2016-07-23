ed672c4eb64e5978d551bc8d59fb9e8848cdf1e1
Vladyslav Hryhorenko <grigorenkovlad1993@gmail.com>
2016-07-23 21:55:59 +0300

    Initial commit

---
+++ WeatherApplicationEntityFramework/WeatherApplication/Controllers/WeatherController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Web.Mvc;
+using WeatherApplication.Services;
+using WeatherApplication.Models;
+using System.Data.Entity;
+using System.Threading.Tasks;
+
+namespace WeatherApplication.Controllers
+{
+    public class WeatherController : Controller
+    {
+        private IWeatherService service;
+        WeatherDataContext bd = new WeatherDataContext();
+        // GET: Weather
+        public WeatherController(IWeatherService _service)
+        {
+            service = _service;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult GetWeather(string city,int term)
+        {
+            var result = service.GetWeatherForecast(city);
+            for (int i = 0; i < term; i++)
+            {
+                ArchieveWeatherData wd = CreateArchieveWeatherData(i, result); 
+                bd.weatherDataRecords.Add(wd);
+            }
+            bd.SaveChanges();
+            ViewBag.days = term;
+            return View(result);
+        }
+
+        private ArchieveWeatherData CreateArchieveWeatherData(int dayNumber, WeatherData.RootObject result)
+        {
+            var data = DateTime.Now;
+            data = data.AddDays(dayNumber);
+            int day = result.list[dayNumber].temp.DayInCelsius();
+            int night = result.list[dayNumber].temp.NightInCelsius();
+            string description = result.list[dayNumber].weather[0].description;
+            return new ArchieveWeatherData(result.city.name, data.Date.ToString("dd/MM/yyyy"), day, night, description);
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult> GetWeatherAsync(string city, int term)
+        {
+
+            var result = await service.GetWeatherForecastAsync(city);
+            for (int i = 0; i < term; i++)
+            {
+                ArchieveWeatherData wd = CreateArchieveWeatherData(i, result);
+                bd.weatherDataRecords.Add(wd);
+            }
+            await bd.SaveChangesAsync();
+            ViewBag.days = term;
+            return View("GetWeather", result);
+        }
+
+        public async Task<ActionResult> GetWeatherHistory()
+        {
+            ViewBag.records = await bd.weatherDataRecords.ToListAsync();
+            return View();
+        }
+
+    }
+}
---
+++ WeatherApplicationEntityFramework/WeatherApplication/Services/WeatherService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Script.Serialization;
+using System.Net;
+using WeatherApplication.Models;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace WeatherApplication.Services
+{
+    public interface IWeatherService
+    {
+        WeatherData.RootObject GetWeatherForecast(string city);
+        Task<WeatherData.RootObject> GetWeatherForecastAsync(string city);
+    }
+
+    public class WeatherService : IWeatherService
+    {
+
+        public WeatherData.RootObject GetWeatherForecast(string city)
+        {
+            WebClient wc = new WebClient();
+            string url = "http://api.openweathermap.org/data/2.5/forecast/daily?q=" + city + "&mode=json&APPID=885b91841cbfc4e0644d236d80d78621";
+            var json = wc.DownloadString(url);
+            var obj = JsonConvert.DeserializeObject<WeatherData.RootObject>(json);
+            return obj;
+        }
+
+        public async Task<WeatherData.RootObject> GetWeatherForecastAsync(string city)
+        {
+            string url = "http://api.openweathermap.org/data/2.5/forecast/daily?q=" + city + "&mode=json&APPID=885b91841cbfc4e0644d236d80d78621";
+            using (HttpClient httpClient = new HttpClient())
+            {
+                return JsonConvert.DeserializeObject<WeatherData.RootObject>
+                (
+                    await httpClient.GetStringAsync(url)
+                );
+            }
+        }
+    }
+}
